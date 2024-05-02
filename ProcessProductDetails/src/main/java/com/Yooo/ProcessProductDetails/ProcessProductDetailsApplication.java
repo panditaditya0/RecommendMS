33ab2c085f42eeddf1d02f503d773801a5d47802
Aditya.Pandit <adpndt@gmail.com>
2024-05-02 14:44:54 +0530

    Added control over download image

--- ProcessProductDetails/src/main/java/com/Yooo/ProcessProductDetails/ProcessProductDetailsApplication.java
+++ ProcessProductDetails/src/main/java/com/Yooo/ProcessProductDetails/ProcessProductDetailsApplication.java
@@ -1,14 +1,20 @@
 package com.Yooo.ProcessProductDetails;
 
+import com.Yooo.ProcessProductDetails.Services.ProductDetailService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
 public class ProcessProductDetailsApplication {
+	private static Logger LOGGER = LoggerFactory.getLogger(ProcessProductDetailsApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProcessProductDetailsApplication.class, args);
+		LOGGER.debug("download_image -> "+ System.getenv("download_image"));
+		LOGGER.debug("download_image_base_url-> "+  System.getenv("download_image_base_url"));
 	}
 
 }
